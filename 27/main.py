e8c10651411094026d0e916439184224cc96f3c2
Iza <lilamovie@wp.pl>
2023-04-24 00:57:38 +0200

    27

--- 27/main.py
+++ 27/main.py
@@ -1,6 +1,25 @@
-def function(*args):
-    for arg in args:
-        print(arg, end=' ')
+import tkinter
+
+def button_click():
+    print(input.get())
 
 
-function(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+window = tkinter.Tk()
+
+window.title("Neural Network")
+window.geometry("500x500")
+# frame = tkinter.Frame(window, width=500, height=500)
+# frame.grid()
+label1 = tkinter.Label(window, text="Welcome to my neural network app!", font="Century 20 bold")
+label1.pack()
+input = tkinter.Entry(window, width=200, font="Century 20 bold")
+input.pack()
+print(input.get())
+
+button1 = tkinter.Button(window, text="Detect language", font="Century 20 bold", command=button_click)
+button1.pack()
+
+button3 = tkinter.Button(window, text="Quit", font="Century 20 bold", command=window.destroy)
+button3.pack()
+
+window.mainloop()
