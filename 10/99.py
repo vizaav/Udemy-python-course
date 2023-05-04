b6df9a924a50e34e5239b36253bc56b7d677370c
Iza <lilamovie@wp.pl>
2023-05-04 23:37:12 +0200

    functions with outputs

---
+++ 10/99.py
@@ -0,0 +1,6 @@
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name."""
+    return f"{f_name.title()} {l_name.title()}"
+
+print(format_name("roBERt", "lEwAnDoWsKi"))
+
