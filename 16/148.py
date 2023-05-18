bc289369d33c003b9462446f912d78cf99db7221
Iza <lilamovie@wp.pl>
2023-05-19 00:37:21 +0200

    packages

--- 16/148.py
+++ 16/148.py
@@ -1,1 +1,15 @@
 import prettytable
+from prettytable import PrettyTable
+
+
+table = PrettyTable(["City name", "Area", "Population", "Annual Rainfall"])
+table.add_row(["Adelaide", 1295, 1158259, 600.5])
+table.add_row(["Brisbane", 5905, 1857594, 1146.4])
+table.add_row(["Darwin", 112, 120900, 1714.7])
+print(table)
+
+table = PrettyTable()
+table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
