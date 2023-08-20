d020b4ab907364b0b3b4ba5e9ba9deea00230113
Iza <lilamovie@wp.pl>
2023-08-20 15:41:01 +0200

    snake game

---
+++ 20/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+screen.update()
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
---
+++ 20/snake.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+
+
+class Snake:
+    def __init__(self):
+        self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+        self.segments = []
+        self.directions = {"up": 90, "down": 270, "left": 180, "right": 0}
+        self.direction = self.directions["right"]
+
+        for position in self.starting_positions:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            self.segments[seg_num].goto(self.segments[seg_num - 1].xcor(), self.segments[seg_num - 1].ycor())
+        self.segments[0].forward(20)
+
+    def up(self):
+        if self.direction != self.directions["down"]:
+            self.direction = self.directions["up"]
+            self.segments[0].setheading(self.direction)
+
+    def down(self):
+        if self.direction != self.directions["up"]:
+            self.direction = self.directions["down"]
+            self.segments[0].setheading(self.direction)
+
+    def left(self):
+        if self.direction != self.directions["right"]:
+            self.direction = self.directions["left"]
+            self.segments[0].setheading(self.direction)
+
+    def right(self):
+        if self.direction != self.directions["left"]:
+            self.direction = self.directions["right"]
+            self.segments[0].setheading(self.direction)
